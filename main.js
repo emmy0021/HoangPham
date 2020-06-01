9e1c36a586719f694f283eb24bf6474046aeb920
Emmanuel Oyervides <emmy0021@gmail.com>
2020-05-31 21:53:39 -0500

    Fix: Desctop View

--- main.js
+++ main.js
@@ -7,19 +7,23 @@ var menuBtn = buttons[1];
 var aboutMeBtn = buttons[2];
 var contactBtn = buttons[3];
 
+var groupBtns = [aboutMeBtn,contactBtn];
+
+console.log(aboutMeBtn.innerHTML);
+
 var ul = document.getElementById('ul');
 
 var mainContent = document.getElementById('mainContent');
 
-var IO = true;
+var open = true;
 
 
 menuBtn.addEventListener('click', function (e) {
-    moveFromTo(mainContent, "20vh", "29vh");
-    clckTextColor(menuTxt);
-    hide(ul);
-    showBorder(menuBtn);
-    IO = !IO;
+    moveFromTo(mainContent, "20vh", "29vh",open);
+    clckTextColor(menuTxt,open);
+    hide(ul,open);
+    showBorder(menuBtn,open);
+    open = !open;
 });
 nameBtn.addEventListener('click', function () {
     //console.log("click " + IO);
@@ -27,42 +31,30 @@ nameBtn.addEventListener('click', function () {
 
     setText(mainContent, "Main page");
     
-    if(!IO){
-        hide(ul);
-        showBorder(menuBtn);
-        clckTextColor(menuTxt);
-        IO = !IO;
+    if(!open){
+        hide(ul,open);
+        showBorder(menuBtn,open);
+        clckTextColor(menuTxt,open);
+        open = !open;
         
-        moveFromTo(mainContent, "29vh", "20vh");
+        moveFromTo(mainContent, "29vh", "20vh",open);
     }
     
 
 });
 
-aboutMeBtn.addEventListener('click', function (e) {
+for(var element = 0;element<groupBtns.length;element++){
+    groupBtns[element].addEventListener('click', function (e) {
 
-        moveFromTo(mainContent, "20vh", "29vh");
-        hide(ul);
-        showBorder(menuBtn);
-        clckTextColor(menuTxt);
-        setText(mainContent, aboutMeBtn.innerHTML);
-        IO = !IO;
-
-});
-
-
-contactBtn.addEventListener('click', function (e) {
-
-        moveFromTo(mainContent, "20vh", "29vh");
-        hide(ul);
-        showBorder(menuBtn);
-        clckTextColor(menuTxt);
-        setText(mainContent, contactBtn.innerHTML);
-        IO = !IO;
+        moveFromTo(mainContent, "20vh", "29vh",open);
+        hide(ul,open);
+        showBorder(menuBtn,open);
+        clckTextColor(menuTxt,open);
+        setText(mainContent, aboutMeBtn.innerHTML,open);
+        open = !open;
 
 });
-
-
+}
 
 
 function setText(e, txt = "") {
@@ -75,8 +67,8 @@ function setText(e, txt = "") {
 
 
 
-function moveFromTo(e, from, to) {
-    if (IO) {
+function moveFromTo(e, from, to, o) {
+    if (o) {
         e.style.top = to;
 
     } else {
@@ -84,33 +76,33 @@ function moveFromTo(e, from, to) {
     }
 }
 
-function clckTextColor(elmnt) {
-    if (IO) {
+function clckTextColor(elmnt,o) {
+    if (o) {
         elmnt.style.color = "red";
     } else {
         elmnt.style.color = "var(--text-color)";
     }
 }
 
-function hide(e) {
-    if (IO) {
+function hide(e,o) {
+    if (open) {
         e.style.visibility = "visible";
     } else {
         e.style.visibility = "hidden";
     }
 }
 
-function showBorder(e) {
-    if (IO) {
+function showBorder(e,o) {
+    if (o) {
         e.style.border = "#C0B8C3 solid 1px";
     } else {
         e.style.border = "#C0B8C3 solid 0px";
     }
 }
 
-function changeBackground(e, color) {
+function changeBackground(e, color,o) {
 
-    if (IO) {
+    if (o) {
         e.style.background = color;
     } else {
         e.style.background = "transparent";
@@ -118,8 +110,8 @@ function changeBackground(e, color) {
     }
 }
 
-function t(e) {
-    if (IO) {
+function t(e,o) {
+    if (o) {
 
     } else {
 
